6a353147f2230c256d697790be2b30ad2feaf198
Ahsan Farooq <ahsan.farooq@yahoo.com>
2020-08-21 21:32:24 -0700

    changes for array size given

--- Q6Equillibrium/Q6Equillibrium/Program.cs
+++ Q6Equillibrium/Q6Equillibrium/Program.cs
@@ -7,38 +7,34 @@ namespace Q6Equillibrium
         static void Main(string[] args)
         {
             int[] arr1 = { -7, 1, 5, 2, -4, 3, 0 };
-            Console.WriteLine(Equilibrium(arr1)); //3
+            Console.WriteLine(Equilibrium(arr1, 7)); //3
             int[] arr2 = { 4, 6, 1, 5, 5 }; //2
-            Console.WriteLine(Equilibrium(arr2));
+            Console.WriteLine(Equilibrium(arr2, 5));
             int[] arr3 = { 4, 6, 1, 5, 2, 4 }; //-1
-            Console.WriteLine(Equilibrium(arr3)); 
-            int[] arr4 = { -1, -1, -1, 0, 1, 1 };
-            Console.WriteLine(Equilibrium(arr4)); 
-            int[] arr5 = { 2, 1, 2, 1 }; //-1
-            Console.WriteLine(Equilibrium(arr5));
-            int[] arr6 = { 5, 2, 3 }; //-1
-            Console.WriteLine(Equilibrium(arr6));
-            int[] arr7 = { 0, 4 };
-            Console.WriteLine(Equilibrium(arr7));
+            Console.WriteLine(Equilibrium(arr3, 6)); 
+            int[] arr4 = { 2, 1, 2, 1 }; //-1
+            Console.WriteLine(Equilibrium(arr4, 4));
+            int[] arr5 = { 5, 2, 3 }; //-1
+            Console.WriteLine(Equilibrium(arr5, 3));
         }
 
-        public static int Equilibrium(int [] a)
+        public static int Equilibrium(int [] arr, int n)
         {
             int arraySum = 0;
-            for (int i =0; i < a.Length; i++)
+            for (int i =0; i < n; i++)
             {
-                arraySum += a[i];
+                arraySum += arr[i];
             }
 
             int leftSum = 0;
-            for (int i = 0; i < a.Length; i++)
+            for (int i = 0; i < n; i++)
             {
-                arraySum -= a[i];
+                arraySum -= arr[i];
                 if(leftSum == arraySum)
                 {
                     return i;
                 }
-                leftSum += a[i];
+                leftSum += arr[i];
             }
             return -1;
         }
