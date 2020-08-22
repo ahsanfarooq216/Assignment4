efb68a982dfdd660ada5d3d13071c7d8b4e31bce
Ahsan Farooq <ahsan.farooq@yahoo.com>
2020-08-22 14:30:38 -0700

    Update Program.cs

--- Q5Sort012/Q5Sort012/Program.cs
+++ Q5Sort012/Q5Sort012/Program.cs
@@ -11,6 +11,43 @@ namespace Q5Sort012
             int[] arr2 = { 1, 2, 0, 2, 1, 0, 0, 2, 1 };
             Sort012(arr2);
         }
+        static void Sort012(int[] a)
+        {
+            int i = 0, j = 0, k = a.Length - 1;
+
+            while (j <= k)
+            {
+                int temp;
+                if (a[j] == 0)
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                    i++;
+                    j++;
+                }
+                else if (a[j] == 1)
+                {
+                    j++;
+                }
+                else
+                {
+                    temp = a[j];
+                    a[j] = a[k];
+                    a[k] = temp;
+                    k--;
+                }
+            }
+            printArray(a);
+        }
+        static void printArray(int [] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write($"{a[i]} ");
+            }
+            Console.WriteLine();
+        }
 
         //static void Sort012(int [] a)
         //{
@@ -48,45 +85,5 @@ namespace Q5Sort012
         //    }
         //    printArray(a);
         //}
-
-        static void Sort012(int[] a)
-        {
-            printArray(a);
-            int i = 0, j = 0, k = a.Length - 1;
-
-            while (j <= k)
-            {
-                int temp;
-                if (a[j] == 0)
-                {
-                    temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                    i++;
-                    j++;
-                }
-                else if (a[j] == 1)
-                {
-                    j++;
-                }
-                else
-                {
-                    temp = a[j];
-                    a[j] = a[k];
-                    a[k] = temp;
-                    k--;
-                }
-            printArray(a);
-            }
-            Console.WriteLine("=================");
-        }
-        static void printArray(int [] a)
-        {
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.Write($"{a[i]} ");
-            }
-            Console.WriteLine();
-        }
     }
 }
