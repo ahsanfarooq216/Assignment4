fc2439d13a661f68bda58cb8a0cf64e00a4f5f74
Ahsan Farooq <ahsan.farooq@yahoo.com>
2020-08-22 16:19:05 -0700

    Q7Leaders

---
+++ Q7Leaders/Q7Leaders/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Q7Leaders
+{
+    class Program
+    {
+        /* Write a program to print all the LEADERS in the array. An element is leader if it is greater than all the elements to its right side.
+         * And the rightmost element is always a leader. For example int the array {16, 17, 4, 3, 5, 2}, leaders are 17, 5 and 2.*/
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 16, 17, 4, 3, 5, 2 };
+            Leaders(arr1); //2 5 17
+            int[] arr2 = { 7, 1, 2, 3, 5, 6, 6, 4 };
+            Leaders(arr2); //2
+            int[] arr3 = { 19 };
+            Leaders(arr3); //19
+        }
+
+        static void Leaders(int [] a)
+        {
+            int leader = int.MinValue;
+            for(int i = a.Length-1; i >= 0; i--)
+            {
+                if(a[i] > leader)
+                {
+                    leader = a[i];
+                    Console.Write($"{leader} ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
